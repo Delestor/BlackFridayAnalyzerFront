0786f2f8b7d36befccd17cc409edcc36c4b51745
Albert Cadena <albert.cad.rub@gmail.com>
2021-04-11 21:30:01 +0200

    Added environment variable for the API URL address

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
 import { Product } from './product';
 
 @Injectable({
@@ -8,7 +9,7 @@ import { Product } from './product';
 })
 
 export class ProductService{
-    private apiServerUrl = '';
+    private apiServerUrl = environment.apiBaseUrl;
 
     constructor(private http: HttpClient){}
 
