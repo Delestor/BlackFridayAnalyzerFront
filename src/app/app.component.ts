b8447285bd68f428c23f8a72086830ad10f71e29
Albert Cadena <albert.cad.rub@gmail.com>
2021-04-11 21:24:42 +0200

    App component get All Products

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { HttpErrorResponse } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { Product } from './product';
+import { ProductService } from './product.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'blackfridayanalyzerapp';
+export class AppComponent implements OnInit{
+  public products : Product[];
+
+  constructor(private productService: ProductService){}
+
+  ngOnInit(){
+    this.getProducts();
+  }
+
+  public getProducts(): void{
+    this.productService.getProducts().subscribe(
+      (response: Product[]) => {
+        this.products = response;
+      },
+      (error: HttpErrorResponse) => {
+        alert (error.message);
+      }
+    );
+  }
 }
