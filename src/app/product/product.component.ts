6b5829063600e11230888f5884df4a2e41f1b608
Albert Cadena <albert.cad.rub@gmail.com>
2021-04-13 15:07:09 +0200

    added Boostrap framework view, and implemented list all products and add product functionallities

--- src/app/product/product.component.ts
+++ src/app/product/product.component.ts
@@ -2,6 +2,7 @@ import { HttpErrorResponse } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { Product } from './product';
 import { ProductService } from './product.service';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-product',
@@ -10,6 +11,8 @@ import { ProductService } from './product.service';
 })
 export class ProductComponent implements OnInit{
   public products : Product[];
+  public editProduct: Product;
+  public deleteProduct: Product;
 
   constructor(private productService: ProductService){}
 
@@ -26,5 +29,55 @@ export class ProductComponent implements OnInit{
         alert (error.message);
       }
     );
+  }
+
+  public onAddProduct(addForm: NgForm): void {
+    document.getElementById('add-product-form').click();
+    this.productService.addProduct(addForm.value).subscribe(
+      (response: Product) => {
+        console.log(response);
+        this.getProducts();
+        addForm.reset();
+      },
+      (error: HttpErrorResponse) => {
+        alert(error.message);
+        addForm.reset();
+      }
+    );
+  }
+
+  public searchProducts(key: string): void {
+    console.log(key);
+    const results: Product[] = [];
+    for (const product of this.products) {
+      if (product.name.toLowerCase().indexOf(key.toLowerCase()) !== -1) {
+        results.push(product);
+      }
+    }
+    this.products = results;
+    if (results.length === 0 || !key) {
+      this.getProducts();
+    }
+  }
+
+  public onOpenModal(product: Product, mode: string): void {
+    const container = document.getElementById('main-container');
+    const button = document.createElement('button');
+    button.type = 'button';
+    button.style.display = 'none';
+    button.setAttribute('data-toggle', 'modal');
+    if (mode === 'add') {
+      button.setAttribute('data-target', '#addProductModal');
+    }
+    if (mode === 'edit') {
+      this.editProduct = product;
+      button.setAttribute('data-target', '#updateProductModal');
+    }
+    if (mode === 'delete') {
+      this.deleteProduct = product;
+      button.setAttribute('data-target', '#deleteProductModal');
+    }
+    container.appendChild(button);
+    button.click();
   }
 }
